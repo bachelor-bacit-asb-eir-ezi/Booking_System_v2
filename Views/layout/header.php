a168ca1a3beb50df779e41a3740e3159aa9d854d
Asbjornhh <89381324+Asbjornhh@users.noreply.github.com>
2023-11-16 22:48:00 +0100

    bootstrappa navbaren

--- Views/layout/header.php
+++ Views/layout/header.php
@@ -3,36 +3,41 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Inkluderer Bootstrap CSS for stilsett og layout -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <!-- Inkluderer jQuery for interaktivitet og Bootstrap JavaScript-komponenter -->
-    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-pzjw8f+ua7Kw1TIq0v8FqFjcJ6pajs/rfdfs3SO+kD5tr5Szkbe5F5Bf5zFNg5dk" crossorigin="anonymous"></script>
-
-    <!--Andre CSS filer-->
-    <link rel="stylesheet" href="../../CSS/style.css">
-
     <title>Booking System</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="../../CSS/style.css">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="/">UIA bookingsystem</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="../user/home.php">Hjem</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="../timeSlot/calender.php">Tidsluker</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="../timeSlot/create.php">Opprett tid</a>
-            </li>
+    <div class="container-fluid">
+        <a class="navbar-brand" href="">UIA bookingsystem</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="../timeSlot/calender.php">Tidsluker</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../timeSlot/create.php">Opprett tid</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../user/home.php">Min profil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../timeSlot/show.php">Booket timer</a>
+                </li>
+            </ul>
 
-        </ul>
+            <form method="POST" class="d-flex">
+                <input class="btn btn-outline-danger" type="submit" name="logOut" value="Logg ut">
+            </form>
+        </div>
     </div>
 </nav>
+
+</body>
+</html>
