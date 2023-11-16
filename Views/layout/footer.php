6146be78db473dcbb9a7f0189c9eb8f8a9b69917
Asbjornhh <89381324+Asbjornhh@users.noreply.github.com>
2023-11-16 22:47:04 +0100

    Til nå er det noe med session som starter som gjør at jeg ikke han logge ut på alle sider med en knapp i navnbaren, dette kan vær et alternativ å bare linke til startPage.php :-)

--- Views/layout/footer.php
+++ Views/layout/footer.php
@@ -1,7 +1,7 @@
 <footer class="bg-light text-center text-lg-start">
     <div class="text-center p-3">
 
-        <a class="text-dark" href="/createTimeSlot">duau</a>
+        <a class="text-dark" href="../startPage.php">eziz e gay</a>
     </div>
 </footer>
 </body>
