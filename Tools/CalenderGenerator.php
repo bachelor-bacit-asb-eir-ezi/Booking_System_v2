0a5a1f52d18a4db146a6875a27e072b3ea2999f0
Eirik <eiriksnedland@gmail.com>
2023-11-30 18:33:22 +0100

    AJAX

--- Tools/CalenderGenerator.php
+++ Tools/CalenderGenerator.php
@@ -4,7 +4,6 @@ require_once(__DIR__ . "/../Controllers/TimeSlotController.php");
 global $week;
 
 
-echo "<div id='calender'>";
 foreach ($week -> getDaysInWeek() as $day){
     $dateDay = DateTime::createFromFormat("Y-m-d", $day -> getDate());
     $formatedDate = $dateDay->format("d-m-Y");
@@ -18,23 +17,22 @@ foreach ($week -> getDaysInWeek() as $day){
         $time = DateTime::createFromFormat("H:i:s" , $timeSlot -> start_time);
         $formatedTime = $time->format("H:i");
         if ($timeSlot -> booked_by === null){
-            echo "<div class='timeSlotStyle availebleTimeSlot cell'>" . $formatedTime . "<br>" . $timeSlot -> tutor_name;
+            echo "<div class='timeSlotStyle availebleTimeSlot cell'>" . $formatedTime . "<br>" . $timeSlot -> tutor_fname . " " . $timeSlot -> tutor_lname;
                 echo "<form method='GET' action='show.php?timeslotId=". $timeSlot -> timeslot_id . 
                         "' id='" . $timeSlot -> timeslot_id . "timeSlotForm'>";
                     echo "<input type='hidden' name='timeSlotId' value='" . $timeSlot -> timeslot_id . "'>";
-                    echo "<button type='submit'> Vis </button>";
+                    echo "<input type='submit' name='getTimeSlotInfo' value='Vis'>";
                 echo "</form>";
             echo "</div>"; 
         } elseif($timeSlot -> booked_by === $_SESSION["user"]["id"] || $timeSlot -> tutor_id === $_SESSION["user"]["id"]) {
-            echo "<div class='timeSlotStyle occupiedTimeSlot cell'>" . $formatedTime . "<br>" . $timeSlot -> tutor_name;
+            echo "<div class='timeSlotStyle occupiedTimeSlot cell'>" . $formatedTime . "<br>" . $timeSlot -> tutor_fname . " " . $timeSlot -> tutor_lname;
                 echo "<form method='GET' action='show.php?timeslotId=". $timeSlot -> timeslot_id . 
                         "' id='" . $timeSlot -> timeslot_id . "timeSlotForm'>";
                     echo "<input type='hidden' name='timeSlotId' value='" . $timeSlot -> timeslot_id . "'>";
-                    echo "<button type='submit'> Vis </button>";
+                    echo "<input type='submit' name='getTimeSlotInfo' value='Vis'>";
                 echo "</form>";
             echo "</div>"; 
         }
     }
 echo "</div>";
 }
-echo "</div>";
