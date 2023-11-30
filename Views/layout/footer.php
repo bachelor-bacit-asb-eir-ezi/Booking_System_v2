ab4cd63a4092d801be8d5ff132c5bfa9c1622b4a
ezizo <ezizomer1999@gmail.com>
2023-11-30 18:25:55 +0100

    head

--- Views/layout/footer.php
+++ Views/layout/footer.php
@@ -1,7 +1,7 @@
 <footer class="bg-light text-center text-lg-start">
     <div class="text-center p-3">
 
-        <a class="text-dark" href="../index.php">eziz e gay</a>
+        <a class="text-dark" href="../index.php"></a>
     </div>
 </footer>
 </body>
