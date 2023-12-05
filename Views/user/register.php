a0c5862f9003f8df19b9d04e059d8ea3d89a9c70
ezizo <ezizomer1999@gmail.com>
2023-12-05 12:47:17 +0100

    feil bruker/pass style

--- Views/user/register.php
+++ Views/user/register.php
@@ -5,34 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrer</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <style>
-        body {
-            background-color: #f8f9fa;
-        }
-
-        .container {
-            background-color: #ffffff;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-            padding: 20px;
-            border-radius: 8px;
-        }
-
-        .form-control {
-            margin-bottom: 15px;
-        }
-
-        img {
-            max-width: 100%;
-            height: auto;
-        }
-    </style>
 </head>
 <body>
     <?php
-    require(__DIR__ . '/../../Controllers/UserController.php');
-
-    
-
+    require_once(__DIR__ . '/../../Controllers/UserController.php');
     ?>
 
     <div class="container mt-5">
@@ -41,7 +17,7 @@
 
                 <img src="../../images/phpReg.png" alt="Registrer bilde" class="img-fluid mb-4">
 
-                <!-- Registration Form -->
+                <!-- Registrerings Form -->
                 <form method="POST" action="">
                     <h2 class="mb-4">Registrer deg her!</h2>
 
